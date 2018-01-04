46bd9c3601f4033d11b246bd3053efcbb7f9b272
XuZihan <xuzihan.boy@gmail.com>
2018-01-04 11:08:50 +0800

    GA
    draw inital path. did not work well

--- GA_main.m
+++ GA_main.m
@@ -10,4 +10,8 @@ Pm=0.05;        %变异概率
 GGAP=0.9;      %代沟(Generation gap)
 D=getDistance(X);  %生成距离矩阵
 N=size(D,1);    %(34*34)
-%%
+%% 初始化种群
+Chrom=InitPop(NIND,N);
+%% 画出随机解的路线图
+DrawPath(Chrom(1,:),X)
+pause(0.0001)
