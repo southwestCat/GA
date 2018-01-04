858d3e10cd3a8ab0fe8e115cba6fd54fcbf0e22e
XuZihan <xuzihan.boy@gmail.com>
2018-01-04 10:37:22 +0800

    1
    1

---
+++ GA_main.m
@@ -0,0 +1,13 @@
+clear
+clc
+close all
+%个城市坐标位置,load schoolPosition.mat
+load('schoolPosition.mat');
+NIND=100;       %种群大小
+MAXGEN=200;
+Pc=0.9;         %交叉概率
+Pm=0.05;        %变异概率
+GGAP=0.9;      %代沟(Generation gap)
+D=getDistance(X);  %生成距离矩阵
+N=size(D,1);    %(34*34)
+%%
